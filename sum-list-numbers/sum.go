fd27227009808c8f11341fc21393df71b395e90c
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2020-10-29 20:06:01 +0000

    Sum a list of numbers - solution

--- sum-list-numbers/sum.go
+++ sum-list-numbers/sum.go
@@ -3,5 +3,15 @@ package sum
 // Sum will sum up all of the numbers passed
 // in and return the result
 func Sum(numbers []int) int {
-	return 0
+	sum := 0
+
+	if len(numbers) == 0 {
+		return 0
+	}
+
+	for _, n := range numbers {
+		sum += n
+	}
+
+	return sum
 }
