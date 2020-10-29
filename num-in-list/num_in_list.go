b390f3d5a294037b654382829762f5dc697607b4
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2020-10-29 19:54:56 +0000

    Find a number in a list of numbers - solution

--- num-in-list/num_in_list.go
+++ num-in-list/num_in_list.go
@@ -3,5 +3,23 @@ package nums
 // NumInList will return true if num is in the
 // list slice, and false otherwise.
 func NumInList(list []int, num int) bool {
+	l := len(list)
+
+	if l == 0 {
+		return false
+	}
+
+	for i := 0; i <= l/2; i++ {
+		if list[i] == num || list[l-i-1] == num {
+			return true
+		}
+	}
+
+	/*for _, n := range list {
+		if n == num {
+			return true
+		}
+	}*/
+
 	return false
 }
