659690bc195ccfae9552ab39ec9c7cc14c4f9f1c
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2020-10-30 22:34:14 +0000

    Bubble sort solution #1

--- bubble_sort/bubble_sort.go
+++ bubble_sort/bubble_sort.go
@@ -4,4 +4,12 @@ package bubble
 //
 // Big O: O(N^2), where N is the size of the list
 func BubbleSortInt(list []int) {
+	len := len(list)
+	for i := 0; i < len-1; i++ {
+		for j := 0; j < len-i-1; j++ {
+			if list[j] > list[j+1] {
+				list[j], list[j+1] = list[j+1], list[j]
+			}
+		}
+	}
 }
